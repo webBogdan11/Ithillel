4719958100a49c4caa822463a1ae79de10d20dcb
Penkov Bohdan <webbogdan11@gmail.com>
2022-10-18 22:55:47 +0300

    added HM9

---
+++ HM9/shop/orders/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from orders.models import Order, Discount
+
+admin.site.register(Order)
+admin.site.register(Discount)
---
+++ HM9/shop/orders/models.py
@@ -0,0 +1,60 @@
+from django.db import models
+
+from decimal import Decimal
+from django.contrib.auth import get_user_model
+
+from shop.constants import MAX_DIGITS, DECIMAL_PLACES
+from shop.mixins.models_mixins import PKMixin
+from shop.model_choices import DiscountTypes
+
+
+class Discount(PKMixin):
+    amount = models.DecimalField(
+        max_digits=MAX_DIGITS,
+        decimal_places=DECIMAL_PLACES,
+        default=0
+    )
+    code = models.CharField(
+        max_length=32
+    )
+    is_active = models.BooleanField(
+        default=True
+    )
+    discount_type = models.PositiveSmallIntegerField(
+        choices=DiscountTypes.choices,
+        default=DiscountTypes.VALUE
+    )
+
+    def __str__(self):
+        return f"{self.amount} | {self.code} | " \
+               f"{DiscountTypes(self.discount_type).label}"
+
+
+class Order(PKMixin):
+    total_amount = models.DecimalField(
+        max_digits=MAX_DIGITS,
+        decimal_places=DECIMAL_PLACES,
+        default=0
+    )
+    user = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+    products = models.ManyToManyField("items.Product")
+    discount = models.ForeignKey(
+        Discount,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+
+    def get_total_amount(self):
+        if self.discount:
+            return (self.total_amount - self.discount.amount
+                    if self.discount.discount_type == DiscountTypes.VALUE else
+                    self.total_amount - (
+                            self.total_amount / 100 * self.discount.amount
+                    )).quantize(Decimal('.01'))
+        return self.total_amount
